0c8c3b4bfde6d67c6fe1d8f89243cda6359f525d
Abdeldjelil BABAHAMED <8852906+babahamed@users.noreply.github.com>
2021-11-04 19:03:13 +0100

    First commit.

---
+++ WispFloat4Unity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WispExtensions
+{
+    public static class WispFloat
+    {
+        public static Vector2 ToVector2(this float ParamMe)
+        {
+            return new Vector2(ParamMe, ParamMe);
+        }
+    }
+}
